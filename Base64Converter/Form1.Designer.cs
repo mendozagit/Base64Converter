0daccc7317e235bf93861a7a8d5009c3f5808c8d
JesusMendoza <mendoza.git@gmail.com>
2021-06-29 16:19:12 -0500

    DevExpress fixes

--- Base64Converter/Form1.Designer.cs
+++ Base64Converter/Form1.Designer.cs
@@ -30,15 +30,13 @@ namespace Base64Converter
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
-            this.TxtName = new DevExpress.XtraEditors.TextEdit();
             this.label3 = new System.Windows.Forms.Label();
-            this.TxtPath = new DevExpress.XtraEditors.TextEdit();
-            this.BtnRestoreFile = new DevExpress.XtraEditors.SimpleButton();
-            this.BtnEncodeFile = new DevExpress.XtraEditors.SimpleButton();
-            this.TxtEncodedFile = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtName.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtPath.Properties)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TxtPath = new System.Windows.Forms.TextBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtEncodedFile = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label2
@@ -51,58 +49,15 @@ namespace Base64Converter
             this.label2.TabIndex = 3;
             this.label2.Text = "File Name";
             // 
-            // TxtName
-            // 
-            this.TxtName.Location = new System.Drawing.Point(13, 108);
-            this.TxtName.Margin = new System.Windows.Forms.Padding(4);
-            this.TxtName.Name = "TxtName";
-            this.TxtName.Size = new System.Drawing.Size(589, 20);
-            this.TxtName.TabIndex = 2;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 132);
+            this.label3.Location = new System.Drawing.Point(18, 145);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Path";
-            // 
-            // TxtPath
-            // 
-            this.TxtPath.Location = new System.Drawing.Point(13, 151);
-            this.TxtPath.Margin = new System.Windows.Forms.Padding(4);
-            this.TxtPath.Name = "TxtPath";
-            this.TxtPath.Size = new System.Drawing.Size(589, 20);
-            this.TxtPath.TabIndex = 4;
-            // 
-            // BtnRestoreFile
-            // 
-            this.BtnRestoreFile.Location = new System.Drawing.Point(301, 233);
-            this.BtnRestoreFile.Margin = new System.Windows.Forms.Padding(4);
-            this.BtnRestoreFile.Name = "BtnRestoreFile";
-            this.BtnRestoreFile.Size = new System.Drawing.Size(301, 53);
-            this.BtnRestoreFile.TabIndex = 7;
-            this.BtnRestoreFile.Text = "Restore file";
-            this.BtnRestoreFile.Click += new System.EventHandler(this.BtnRestoreFile_Click);
-            // 
-            // BtnEncodeFile
-            // 
-            this.BtnEncodeFile.Location = new System.Drawing.Point(13, 233);
-            this.BtnEncodeFile.Margin = new System.Windows.Forms.Padding(4);
-            this.BtnEncodeFile.Name = "BtnEncodeFile";
-            this.BtnEncodeFile.Size = new System.Drawing.Size(280, 53);
-            this.BtnEncodeFile.TabIndex = 8;
-            this.BtnEncodeFile.Text = "Encode file";
-            this.BtnEncodeFile.Click += new System.EventHandler(this.BtnEncodeFile_Click);
-            // 
-            // TxtEncodedFile
-            // 
-            this.TxtEncodedFile.Location = new System.Drawing.Point(12, 50);
-            this.TxtEncodedFile.Name = "TxtEncodedFile";
-            this.TxtEncodedFile.Size = new System.Drawing.Size(590, 22);
-            this.TxtEncodedFile.TabIndex = 9;
             // 
             // label4
             // 
@@ -114,26 +69,65 @@ namespace Base64Converter
             this.label4.TabIndex = 10;
             this.label4.Text = "Encoded file";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 55);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Encode file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(273, 55);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Restore file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TxtPath
+            // 
+            this.TxtPath.Location = new System.Drawing.Point(16, 164);
+            this.TxtPath.Name = "TxtPath";
+            this.TxtPath.Size = new System.Drawing.Size(581, 22);
+            this.TxtPath.TabIndex = 13;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(16, 104);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(581, 22);
+            this.TxtName.TabIndex = 14;
+            // 
+            // TxtEncodedFile
+            // 
+            this.TxtEncodedFile.Location = new System.Drawing.Point(16, 50);
+            this.TxtEncodedFile.Name = "TxtEncodedFile";
+            this.TxtEncodedFile.Size = new System.Drawing.Size(581, 22);
+            this.TxtEncodedFile.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(629, 354);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.TxtEncodedFile);
-            this.Controls.Add(this.BtnEncodeFile);
-            this.Controls.Add(this.BtnRestoreFile);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.TxtPath);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtPath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.TxtName.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtPath.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,13 +135,13 @@ namespace Base64Converter
 
         #endregion
         private System.Windows.Forms.Label label2;
-        private DevExpress.XtraEditors.TextEdit TxtName;
         private System.Windows.Forms.Label label3;
-        private DevExpress.XtraEditors.TextEdit TxtPath;
-        private DevExpress.XtraEditors.SimpleButton BtnRestoreFile;
-        private DevExpress.XtraEditors.SimpleButton BtnEncodeFile;
-        private System.Windows.Forms.TextBox TxtEncodedFile;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox TxtPath;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtEncodedFile;
     }
 }
 
--- Base64Converter/Form1.cs
+++ Base64Converter/Form1.cs
@@ -20,35 +20,13 @@ namespace Base64Converter
 
         private void BtnEncodeFile_Click(object sender, EventArgs e)
         {
-            var path = GetFilePath().Item1;
-            TxtPath.Text = path;
-            var base64File = GetBase64FromFilePath(path);
-            TxtEncodedFile.Text = base64File;
-
-            MessageBox.Show("Done.");
+          
         }
 
         private void BtnRestoreFile_Click(object sender, EventArgs e)
         {
 
-            if (string.IsNullOrEmpty(TxtEncodedFile.Text))
-            {
-                MessageBox.Show("TxtEncodedFile is requiered.");
-                return;
-            }
-            if (string.IsNullOrEmpty(TxtName.Text))
-            {
-                MessageBox.Show("Name is requiered.");
-                return;
-            }
-            var path = GetFolderPath();
-            TxtPath.Text = path;
-
-            var fullFileName = $"{path}{TxtName.Text.Trim()}";
-            //var base64File = GetBase64FromFilePath(path);
-            File.WriteAllBytes(fullFileName, Convert.FromBase64String(TxtEncodedFile.Text));
-
-            MessageBox.Show("Done.");
+           
 
         }
         public static string GetFolderPath()
@@ -108,6 +86,38 @@ namespace Base64Converter
         private void simpleButton1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = GetFilePath().Item1;
+            TxtPath.Text = path;
+            var base64File = GetBase64FromFilePath(path);
+            TxtEncodedFile.Text = base64File;
+
+            MessageBox.Show("Done.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtEncodedFile.Text))
+            {
+                MessageBox.Show("TxtEncodedFile is requiered.");
+                return;
+            }
+            if (string.IsNullOrEmpty(TxtName.Text))
+            {
+                MessageBox.Show("Name is requiered.");
+                return;
+            }
+            var path = GetFolderPath();
+            TxtPath.Text = path;
+
+            var fullFileName = $"{path}{TxtName.Text.Trim()}";
+            //var base64File = GetBase64FromFilePath(path);
+            File.WriteAllBytes(fullFileName, Convert.FromBase64String(TxtEncodedFile.Text));
+
+            MessageBox.Show("Done.");
         }
     }
 }
